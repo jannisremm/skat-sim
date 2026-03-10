a7167756ee1031f8ea7aabc4e3df3caa36729372
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-03-10 08:46:38 +0100

    feat: :sparkles: Add method to deal cards

--- src/table.py
+++ src/table.py
@@ -1,5 +1,10 @@
 """This class handles the cards when they are not in the hands of the players"""
 
+import random
+
+from cards import create_deck
+from player import Player
+
 
 class Table:
     def __init__(self) -> None:
@@ -10,6 +15,18 @@ class Table:
         """shuffles the list of cards"""
         pass
 
-    def deal_cards(self):
+    def deal_cards(self, player1: Player, player2: Player, player3: Player):
         """Deals the cards to the three players and the skat to the table"""
-        pass
+        cards = create_deck()
+        random.shuffle(cards)
+        for player in [player1, player2, player3]:
+            for _ in range(3):
+                player.cards.append(cards.pop())
+        self.skat.append(cards.pop())
+        self.skat.append(cards.pop())
+        for player in [player1, player2, player3]:
+            for _ in range(4):
+                player.cards.append(cards.pop())
+        for player in [player1, player2, player3]:
+            for _ in range(3):
+                player.cards.append(cards.pop())
