b58abcede17f41ecd1ac385200d53b20bea9f03d
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-03-10 10:55:41 +0100

    feat: :sparkles: Improve play_card function to follow suit

--- src/player.py
+++ src/player.py
@@ -7,6 +7,20 @@ class Player:
         self.cards = []
         self.points = 0
 
-    def play_card(self):
-        """Choses the best available legal card to play"""
-        pass
+    def play_card(self, cards_on_table):
+        """Choses the best available legal card to play
+        currently uses random logic"""
+        if len(cards_on_table) == 0:
+            # The first card that is played can be random
+            card_to_play = self.cards.pop()
+            return card_to_play
+
+        # The second and third players need to follow suit
+        suit_to_follow = cards_on_table[0].suit
+        legal_cards = [card for card in self.cards if card.suit == suit_to_follow]
+        if legal_cards:
+            card_to_play = legal_cards.pop()
+            self.cards.remove(card_to_play)
+        card_to_play = self.cards.pop()
+
+        return card_to_play
