e8bb4ab6bcca5abae75a7ff919cbd5721ac140c5
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-03-10 17:47:28 +0100

    feat: :sparkles: Update logic to accomodate trump_suit

--- src/table.py
+++ src/table.py
@@ -10,6 +10,7 @@ class Table:
     def __init__(self) -> None:
         self.skat = []
         self.current_hand = []
+        self.trump_suite = None
 
     def shuffle_cards(self):
         """shuffles the list of cards"""
@@ -34,12 +35,24 @@ class Table:
     def start_game(self, player1: Player, player2: Player, player3: Player):
         chosen_player = random.choice([player1, player2, player3])
         chosen_player.take_skat(self.skat)
-        chosen_player.determimne_trump_suit()
+        self.skat = []
+        self.trump_suite = chosen_player.determimne_trump_suit()
 
     def play_round(self, player1: Player, player2: Player, player3: Player):
         self.current_hand = []
-        self.current_hand.append((player1.name, player1.play_card(self.current_hand)))
-        self.current_hand.append((player2.name, player2.play_card(self.current_hand)))
-        self.current_hand.append((player3.name, player3.play_card(self.current_hand)))
+        self.current_hand.append(
+            (player1.name, player1.play_card(self.current_hand, self.trump_suite))
+        )
+        self.current_hand.append(
+            (player2.name, player2.play_card(self.current_hand, self.trump_suite))
+        )
+        self.current_hand.append(
+            (player3.name, player3.play_card(self.current_hand, self.trump_suite))
+        )
 
         # evaluate which player has won the trick
+        """Pseudocode:
+        if one or more players have played a trump card:
+            highest trump card wins
+        else:
+            highest played card of the suit of the first played card wins"""
