5d3d2cf7f4857405d0e734445f7e50034609edb5
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-03-10 09:02:08 +0100

    feat: :sparkles: Set up initial game state

--- src/game_logic_flow.py
+++ src/game_logic_flow.py
@@ -1,1 +1,17 @@
 """This file handles the game flow, determines which players turn it is etc, and calculates points at the end"""
+
+from player import Player
+from table import Table
+
+horst = Player("Horst")
+ulf = Player("Ulf")
+gunni = Player("Gunni")
+
+tisch = Table()
+
+tisch.deal_cards(horst, ulf, gunni)
+
+print(horst.cards)
+print(ulf.cards)
+print(gunni.cards)
+print(tisch.skat)
